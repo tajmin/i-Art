9f6ca2a84c6c39279bb5cc7e2b64a4f77286ff30
tajmin <tajminur.aiub@gmail.com>
2021-10-04 02:21:58 +0600

    header, footer, font awesome

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import PageNotFound from './components/PageNotFound/PageNotFound';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
       <BrowserRouter>
+        <Header></Header>
         <Switch>
 
           <Route path="/*">
             <PageNotFound></PageNotFound>
           </Route>
         </Switch>
+        <Footer></Footer>
       </BrowserRouter>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../../images/Capture.png'
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is header</h1>
+        <div className="bg-yellow-500">
+            <div className="container mx-auto relative p-6">
+                <nav className="relative flex items-center sm:h-10 lg:justify-between sm:m-4 lg:m-8 lg:w-4/5">
+                    <div>
+                        <img src={logo} alt="" />
+                    </div>
+                    <div className="md:block md:ml-10 md:pr-4 md:space-x-8 font-bold text-xl">
+                        <NavLink className="font-medium text-white hover:text-red-600" to="/home">
+                            Home
+                        </NavLink>
+                        <NavLink className="font-medium text-white hover:text-red-600" to="/about">
+                            About
+                        </NavLink>
+                        <NavLink className="font-medium text-white hover:text-red-600" to="/services">
+                            Services
+                        </NavLink>
+                        <NavLink className="font-medium text-white hover:text-red-600" to="/contact">
+                            Contact
+                        </NavLink>
+
+                    </div>
+
+                </nav>
+            </div>
         </div>
     );
 };
