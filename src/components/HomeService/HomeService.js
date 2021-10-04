1cfe5ec63c34f0a9af826e8ab942cb3b9bad9ec4
tajmin <tajminur.aiub@gmail.com>
2021-10-04 14:26:20 +0600

    Home full

--- src/components/HomeService/HomeService.js
+++ src/components/HomeService/HomeService.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import useCourses from '../../hooks/useCourses';
 import Course from '../Course/Course';
 
 const HomeService = () => {
-    const [courses, setCourses] = useCourses();
+    const [courses] = useCourses();
     return (
-        <div>
-            {
-                courses.map(course => <Course
-                    course={course}
-                    key={course._id}>
-                </Course>).slice(0, 4)
-            }
+        <div className="bg-yellow-400 font-serif ">
+            <h1 className="sm:text-2xl lg:text-5xl capitalize text-white pt-20">what can we offer</h1>
+            <div className="border w-28 mx-auto mt-3"></div>
+            <div className="container mx-auto p-8 grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-8">
+                {
+                    courses.map(course => <Course
+                        course={course}
+                        key={course._id}>
+                    </Course>).slice(0, 4)
+                }
+            </div>
+            <div className="relative py-12">
+                <div className="lg:absolute lg:bottom-0 lg:right-0 bg-black bg-opacity-50 py-4 mr-4 mb-8">
+                    <NavLink to="/services" className="py-8 px-8 text-xl text-white hover:text-black">See More</NavLink>
+                </div>
+            </div>
         </div>
     );
 };
